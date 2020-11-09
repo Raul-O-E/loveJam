a86306849c2165e12d2e1c1998e9926c006911f5
Raul Oliva Escribano <raul.oliva.escribano@gmail.com>
2020-11-09 16:08:17 +0100

    background

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.window.width = 720
-  t.window.height = 1024
+  t.window.height = 850
   t.console = true
   t.title = "LoveJam"
 end
--- main.lua
+++ main.lua
@@ -2,15 +2,25 @@ actorList = {}  --Lista de elementos de juego
 math.randomseed(os.time())
 
 function love.load()
+  actorList = {} 
+  backgroundImage = love.graphics.newImage("Textures/background.png")
+  backgroundImage2 = love.graphics.newImage("Textures/background.png")
+  backgroundImage1Y = 0
+  backgroundImage2Y = 1024
   
 end
 
 function love.update(dt)
-  
+  for _,v in ipairs(actorList) do
+    v:update(dt)
+  end
+  backgroundImage1Y = backgroundImage1Y + 1 * dt
+  backgroundImage2Y = backgroundImage2Y + 1 * dt
 end
 
 function love.draw()
-  
+  love.graphics.draw(backgroundImage,0,backgroundImage1Y)
+  love.graphics.draw(backgroundImage2,0, backgroundImage2Y)
 end
 
 function love.keypressed(key)
