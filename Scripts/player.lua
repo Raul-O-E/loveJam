fec6c163a28642d5bf43546fc91b31a76884d7e2
Raul Oliva Escribano <raul.oliva.escribano@gmail.com>
2020-11-14 18:31:46 +0100

    playerMovement y olapng

--- Scripts/player.lua
+++ Scripts/player.lua
@@ -1,9 +1,13 @@
 local Actor = Actor or require "Scripts/actor"
+local Data = Data or require "data"
 local Player = Actor:extend()
+
 local positionX=360
 local positionY=200
 local motoScale=0.3
+local playerSpeed=170
 local motoAnguloCorrecto=45.55
+
 function Player:new(x,y)
   Player.super.new(self,"Textures/moto.png",positionX,positionY,50,1,0)
 end
@@ -12,21 +16,22 @@ function Player:update(dt)
   Player.super.update(self,dt)  
   if love.keyboard.isDown("down")then  
     
-      positionY=positionY+1
+      positionY=positionY + playerSpeed * dt
   end
   if love.keyboard.isDown("up")then  
     
-      positionY=positionY-1
+      positionY=positionY - playerSpeed * dt
   end
   if love.keyboard.isDown("left")then  
-    
-      positionX=positionX-1
+    if positionX > 200 then  
+      positionX=positionX - playerSpeed * dt
+    end
   end
   if love.keyboard.isDown("right")then  
-    
-      positionX=positionX+1
+    if positionX < 520 then  
+      positionX=positionX + playerSpeed * dt
+    end
   end
-   
 end
 
 function Player:draw() 
