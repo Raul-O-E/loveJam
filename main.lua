6a8662eb55a1e297cc4c65df73e0bf5332c0c909
Raul Oliva Escribano <raul.oliva.escribano@gmail.com>
2020-11-14 18:57:18 +0100

    ObstacleSpawner

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 actorList = {}  --Lista de elementos de juego
 local Player = Player or require "Scripts/player"
+local Data = Data or require "data"
 local Box = Box or require "Scripts/box"
 local Wave = Wave or require "Scripts/wave"
 local Basura = Basura or require "Scripts/basura"
 math.randomseed(os.time())
+
+timerSpawn = 0
 
 function love.load()
   actorList = {}
@@ -11,13 +14,26 @@ function love.load()
   backgroundImage2 = love.graphics.newImage("Textures/background.png")
   backgroundImage1Y = 0
   backgroundImage2Y = 1275
-  local b = Basura()
-  table.insert(actorList,b)
   local p = Player()  
   table.insert(actorList,p)
 end
 
 function love.update(dt)
+  timerSpawn = timerSpawn + dt
+  if timerSpawn >= SPAWN_RATE then
+    timerSpawn = 0
+    obstacleRnd = math.random(3)
+    if obstacleRnd == 1 then
+      x = Box:extend()
+    elseif obstacleRnd == 2 then
+      x = Basura:extend()
+    elseif obstacleRnd == 3 then
+      x = Basura:extend()
+    end
+    x:new()
+    table.insert(actorList,x)
+  end
+  
   for _,v in ipairs(actorList) do
     v:update(dt)
   end
